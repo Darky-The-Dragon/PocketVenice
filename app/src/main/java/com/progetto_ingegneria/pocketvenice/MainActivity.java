ba67aaadc7e0705dc6c176a8cb215804cec44056
JayRen432 <881142@stud.unive.it>
2022-11-19 14:40:51 +0100

    19-11-22
    Add Nav-bar
    by Nicolò Bregantin

--- app/src/main/java/com/progetto_ingegneria/pocketvenice/MainActivity.java
+++ app/src/main/java/com/progetto_ingegneria/pocketvenice/MainActivity.java
@@ -7,29 +7,69 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.progetto_ingegneria.pocketvenice.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView textViewLogout;
     ProgressBar progressBar;
+    ActivityMainBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        replaceFragment(new News_Fragment());
+
         setContentView(R.layout.activity_main);
-
-        textViewLogout = findViewById(R.id.logout);
+        textViewLogout = findViewById(R.id.login);
         textViewLogout.setOnClickListener(this);
-
         progressBar = findViewById(R.id.progress_bar);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.news:
+                    replaceFragment(new News_Fragment());
+                    break;
+                case R.id.events:
+                    replaceFragment(new Events_Fragment());
+                    break;
+                case R.id.place:
+                    replaceFragment(new Place_Fragment());
+                    break;
+                case R.id.maps:
+                    replaceFragment(new Map_Fragment());
+                    break;
+            }
+            return true;
+        });
+
+
+
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.logout) {
+        if (v.getId() == R.id.login) {
             progressBar.setVisibility(View.VISIBLE);
             FirebaseAuth.getInstance().signOut();
             progressBar.setVisibility(View.GONE);
