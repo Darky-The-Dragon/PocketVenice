8c2f40d67e953abb7fa702bb1aa9aa30ccf7909f
LeonardoB25 <4GD4S89;~Pf.7T>
2022-12-07 12:06:37 +0100

    aggiunta immagini a database e lister per il click sui posti in lavorazione

--- app/src/main/java/com/progetto_ingegneria/pocketvenice/BottomNavbarActivities/Places/MyAdapter.java
+++ app/src/main/java/com/progetto_ingegneria/pocketvenice/BottomNavbarActivities/Places/MyAdapter.java
@@ -18,6 +18,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     Context context;
     ArrayList<Place_Data> list;
 
+    private OnRecyclerViewClickListener listener;
+
+    public interface OnRecyclerViewClickListener{
+        void OnItemClick(int position);
+    }
+
+    public void OnRecyclerViewClickListener (OnRecyclerViewClickListener listener){
+        this.listener = listener;
+    }
+
     public MyAdapter(Context context, ArrayList<Place_Data> list) {
         this.context = context;
         this.list = list;
@@ -26,8 +36,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.news_list_items, parent, false);
-        return new MyViewHolder(v);
+        View v = LayoutInflater.from(context).inflate(R.layout.places_list_items, parent, false);
+        return new MyViewHolder(v, listener);
     }
 
     @Override
@@ -43,12 +53,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView titolo, sestiere;
+        TextView titolo, sestiere, foto;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             titolo = itemView.findViewById(R.id.text_titolo);
             sestiere = itemView.findViewById(R.id.text_sestiere);
+            foto = itemView.findViewById(R.id.img_place);
         }
     }
 
