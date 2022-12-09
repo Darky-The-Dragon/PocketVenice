ad101f57e662d96ffd26ec1daa8b95c59409bf98
Darky <887372@stud.unive.it>
2022-12-09 17:14:36 +0100

    Changelog: - Added MainActivity.java and activity_main.xml for future conversion into a fragment architecture - Optimized library imports - Code cleanup
    Collaborators:
    - Zotea Dumitru

---
+++ app/src/main/java/com/progetto_ingegneria/pocketvenice/MainActivity.java
@@ -0,0 +1,153 @@
+package com.progetto_ingegneria.pocketvenice;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.progetto_ingegneria.pocketvenice.Auth.LoginActivity;
+import com.progetto_ingegneria.pocketvenice.BottomNavbarActivities.EventsActivity;
+import com.progetto_ingegneria.pocketvenice.BottomNavbarActivities.MapsActivity;
+import com.progetto_ingegneria.pocketvenice.BottomNavbarActivities.News.NewsActivity;
+import com.progetto_ingegneria.pocketvenice.BottomNavbarActivities.Places.PlacesActivity;
+import com.progetto_ingegneria.pocketvenice.LateralNavbar.FAQ;
+import com.progetto_ingegneria.pocketvenice.LateralNavbar.Info;
+import com.progetto_ingegneria.pocketvenice.LateralNavbar.Profile;
+import com.progetto_ingegneria.pocketvenice.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textTitle;
+    private ImageView imageMenu;
+    private BottomNavigationView bottomNavigationView;
+    private NavigationView navigationView;
+    private NavController navController;
+    private NavController bottomNavController;
+    private DrawerLayout drawerLayout;
+    private ActivityMainBinding binding;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+
+
+        setContentView(binding.getRoot());
+        drawerLayout = findViewById(R.id.drawerLayout);
+        //replaceFragment(new NewsActivity());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressBar = findViewById(R.id.progress_bar);
+
+        textTitle = findViewById(R.id.menu_title);
+        textTitle.setText(NewsActivity.class.getSimpleName());
+
+        imageMenu = findViewById(R.id.menu_nav);
+        imageMenu.setOnClickListener(this);
+
+        //drawerLayout = findViewById(R.id.drawerLayout);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.news) {
+                //replaceFragment(new NewsActivity());
+                textTitle.setText(NewsActivity.class.getSimpleName());
+            } else if (item.getItemId() == R.id.events) {
+                //replaceFragment(new EventsActivity());
+                textTitle.setText(EventsActivity.class.getSimpleName());
+            } else if (item.getItemId() == R.id.places) {
+                //replaceFragment(new PlacesActivity());
+                textTitle.setText(PlacesActivity.class.getSimpleName());
+            } else if (item.getItemId() == R.id.map) {
+                //replaceFragment(new MapsActivity());
+                textTitle.setText(MapsActivity.class.getSimpleName());
+            }
+
+            return true;
+        });
+
+        binding.navigationView.setNavigationItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.profile) {
+                replaceFragment(new Profile());
+                textTitle.setText(Profile.class.getSimpleName());
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else if (item.getItemId() == R.id.faq) {
+                replaceFragment(new FAQ());
+                textTitle.setText(FAQ.class.getSimpleName());
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else if (item.getItemId() == R.id.info) {
+                replaceFragment(new Info());
+                textTitle.setText(Info.class.getSimpleName());
+                drawerLayout.closeDrawer(GravityCompat.START);
+            } else if (item.getItemId() == R.id.logout) {
+                logoutUser();
+            }
+
+            return true;
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.menu_nav) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.frame_layout);
+        if(fragment != null){
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(fragment);
+            fragmentTransaction.commit();
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    private void logoutUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        FirebaseAuth.getInstance().signOut();
+        progressBar.setVisibility(View.GONE);
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+}
