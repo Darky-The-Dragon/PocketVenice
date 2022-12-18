4b1bed927fc7a76c25aba128a9a338ab28f1e7f2
Darky <887372@stud.unive.it>
2022-12-18 18:31:20 +0100

    Changelog: - Added API Query in a separate interface - Added a listener for Events - Minor changes in main Activity for setting up onClickListeners - Fixed minor warnings - Optimized folder hierarchy - Optimized library imports - Code cleanup
    Collaborators:
    - Zotea Dumitru

--- app/src/main/java/com/progetto_ingegneria/pocketvenice/MainActivity.java
+++ app/src/main/java/com/progetto_ingegneria/pocketvenice/MainActivity.java
@@ -2,11 +2,13 @@ package com.progetto_ingegneria.pocketvenice;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -16,6 +18,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.progetto_ingegneria.pocketvenice.Auth.LoginActivity;
@@ -33,70 +36,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationBarView.OnItemSelectedListener, NavigationView.OnNavigationItemSelectedListener {
 
     protected TextView textTitle;
     protected ImageView imageMenu;
     protected BottomNavigationView bottomNavigationView;
     protected NavigationView navigationView;
-    protected NavController navController;
-    protected NavController bottomNavController;
     protected DrawerLayout drawerLayout;
     protected ActivityMainBinding binding;
     protected ProgressBar progressBar;
-    protected FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
         initview();
         setup();
-        mAuth = FirebaseAuth.getInstance();
-
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
-            if (item.getItemId() == R.id.news) {
-                replaceFragment(new TestNews());
-                textTitle.setText(R.string.news);
-            } else if (item.getItemId() == R.id.events) {
-                replaceFragment(new TestEvents());
-                textTitle.setText(R.string.events);
-            } else if (item.getItemId() == R.id.places) {
-                replaceFragment(new TestPlaces());
-                textTitle.setText(R.string.place);
-            } else if (item.getItemId() == R.id.map) {
-                replaceFragment(new TestMaps());
-                textTitle.setText(R.string.map);
-            }
-
-            return true;
-        });
-
-        binding.navigationView.setNavigationItemSelectedListener(item -> {
-
-            if (item.getItemId() == R.id.profile) {
-                replaceFragment(new Profile());
-                textTitle.setText(Profile.class.getSimpleName());
-                drawerLayout.closeDrawer(GravityCompat.START);
-            } else if (item.getItemId() == R.id.faq) {
-                replaceFragment(new FAQ());
-                textTitle.setText(FAQ.class.getSimpleName());
-                drawerLayout.closeDrawer(GravityCompat.START);
-            } else if (item.getItemId() == R.id.info) {
-                replaceFragment(new Info());
-                textTitle.setText(Info.class.getSimpleName());
-                drawerLayout.closeDrawer(GravityCompat.START);
-            }else if (item.getItemId() == R.id.guide) {
-                startActivity(new Intent(this, GuideActivity.class));
-            } else if (item.getItemId() == R.id.logout) {
-                logoutUser();
-            }
-
-            return true;
-        });
     }
 
     private void setup() {
@@ -115,7 +71,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imageMenu = findViewById(R.id.menu_nav);
         drawerLayout = findViewById(R.id.drawerLayout);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
         navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -153,5 +111,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FirebaseAuth.getInstance().signOut();
         progressBar.setVisibility(View.GONE);
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.news) {
+            replaceFragment(new TestNews());
+            textTitle.setText(R.string.news);
+        } else if (item.getItemId() == R.id.events) {
+            replaceFragment(new TestEvents());
+            textTitle.setText(R.string.events);
+        } else if (item.getItemId() == R.id.places) {
+            replaceFragment(new TestPlaces());
+            textTitle.setText(R.string.place);
+        } else if (item.getItemId() == R.id.map) {
+            replaceFragment(new TestMaps());
+            textTitle.setText(R.string.map);
+        } else if (item.getItemId() == R.id.profile) {
+            replaceFragment(new Profile());
+            textTitle.setText(Profile.class.getSimpleName());
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (item.getItemId() == R.id.faq) {
+            replaceFragment(new FAQ());
+            textTitle.setText(FAQ.class.getSimpleName());
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (item.getItemId() == R.id.info) {
+            replaceFragment(new Info());
+            textTitle.setText(Info.class.getSimpleName());
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else if (item.getItemId() == R.id.guide) {
+            startActivity(new Intent(this, GuideActivity.class));
+        } else if (item.getItemId() == R.id.logout) {
+            logoutUser();
+        }
+
+        return true;
     }
 }
