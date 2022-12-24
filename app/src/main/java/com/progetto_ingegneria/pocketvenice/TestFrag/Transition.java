f13bfe879c5727940edd41aa60f00b0ea04aac3a
Jay Ren <881142@stud.unive.it>
2022-12-24 13:20:51 +0100

    Changelog
    1. Add Transition.java file. This class is used to set new transition from fragment A to fragment B
    2.Fix addSharedElement in TestEvents. Now there isn't unique transition name exception
    3. Add in TestEvents setTransitionName.
    4. Do the 2 & 3 point in TestPlaces class

---
+++ app/src/main/java/com/progetto_ingegneria/pocketvenice/TestFrag/Transition.java
@@ -0,0 +1,25 @@
+package com.progetto_ingegneria.pocketvenice.TestFrag;
+
+import android.content.Context;
+import android.transition.ChangeBounds;
+import android.transition.ChangeImageTransform;
+import android.transition.ChangeTransform;
+import android.transition.TransitionSet;
+import android.util.AttributeSet;
+
+public class Transition extends TransitionSet {
+    public Transition(){
+        init();
+    }
+    public Transition(Context context, AttributeSet atts){
+        super(context, atts);
+        init();
+    }
+
+    private void init(){
+        setOrdering(ORDERING_TOGETHER);
+        addTransition(new ChangeBounds())
+                .addTransition(new ChangeTransform())
+                .addTransition(new ChangeImageTransform());
+    }
+}
